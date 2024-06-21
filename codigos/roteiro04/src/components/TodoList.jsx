c7dd7fdf9b33c4a398b8eeb3c17b94b59281fec7
Maria Eduarda <104326131+dudachrispim@users.noreply.github.com>
2024-06-21 20:54:06 -0300

    Update TodoList.jsx

--- codigos/roteiro04/src/components/TodoList.jsx
+++ codigos/roteiro04/src/components/TodoList.jsx
@@ -1,15 +1,28 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash, faPenToSquare } from '@fortawesome/free-solid-svg-icons'
-import React from 'react'
-export const TodoList = ({task}) => {
-return (
-<div className="Todo">
-<p className={`${task.completed ? "completed" : "incompleted"}`}
->{task.description}</p>
-<div>
-<FontAwesomeIcon className="delete-icon" icon={faTrash}/>
-<FontAwesomeIcon className="edit-icon" icon={faPenToSquare} />
-</div>
-</div>
-)
-}
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+
+export const TodoList = ({ task, deleteTodo, editTodo, toggleComplete }) => {
+  return (
+    <div className="Todo">
+      <p
+        className={`${task.completed ? "completed" : "incompleted"}`}
+        onClick={() => toggleComplete(task.id)}
+      >
+        {task.description}
+      </p>
+      <div>
+        <FontAwesomeIcon
+          className="delete-icon"
+          icon={faTrash}
+          onClick={() => deleteTodo(task.id)}
+        />
+        <FontAwesomeIcon
+          className="edit-icon"
+          icon={faPenToSquare}
+          onClick={() => editTodo(task.id)}
+        />
+      </div>
+    </div>
+  );
+};
